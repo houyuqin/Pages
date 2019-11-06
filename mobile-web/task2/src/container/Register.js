e45687b1973f655ae593b5c713b9890eae6c7939
houyuqin <1738519809@qq.com>
2019-11-06 11:30:27 +0800

    second revise register

--- mobile-web/task2/src/container/Register.js
+++ mobile-web/task2/src/container/Register.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import {Link,Route} from 'react-router-dom'
+import All from './Home/All'
 
 export default class Register extends Component {
     render() {
         return (
+            <div>
             <div className='lgi'>
                 用户名：<input type="text" className="txt"></input>
                 <br/>
                 密码：<input type="password" className="pwd"></input>
                 <br/>
                 <Link to='/all'>登录</Link>
+            </div>
+            <div>
+            <Route path='/all' component={All}/>
+            </div>
             </div>
         )
     }
